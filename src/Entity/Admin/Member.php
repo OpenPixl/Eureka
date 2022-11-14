08c5423d41a167eeffc3c2206111add063f829c7
Assin764 <amiri.assin123@gmail.com>
2022-11-14 18:50:14 +0100

    Ajout vue cours

--- src/Entity/Admin/Member.php
+++ src/Entity/Admin/Member.php
@@ -420,4 +420,8 @@ class Member implements UserInterface, PasswordAuthenticatedUserInterface
 
         return $this;
     }
+
+    public function __toString(){
+        return $this->lastName;
+    }
 }
